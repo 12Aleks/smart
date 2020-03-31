f02b1e13e6a7f005be4947224b76301d307cc93a
12tanais <leszek.koba@termagroup.pl>
2020-03-31 10:44:31 +0200

    add new element

--- java.js
+++ java.js
@@ -1,19 +1,39 @@
+let $handBox = $('.hand_box'),
+     $nav = $('#navigation'),
+      $myDiv = $(".img-room");
+$(window).resize(function () {
+    let $myChild = $myDiv.find("> *:first-child");
+    $myDiv.animate({
+        scrollLeft: ($myChild.width() - $myDiv.width()) / 2
+    }, {
+        duration: 0
+    });
+});
+$(window).resize();
+
 $(document).ready(function () {
 
     $('.carousel').carousel({
         interval: 4000
     });
 
-    $("#video-carousel-example").on('slide.bs.carousel', function () {
-           var $default = $(this).find(".carousel-item"),
-               $active = $(this).find('.active > video');
+    // $('.video-fluid').find('video').each(function () {
+    //     this[0].pause();
+    // });
+    //
+    // $('.active').find('video').get(0).currentTime = 0;
+    // $('.active').find('video').each(function () {
+    //     this.play();
+    // });
 
-        if($default.hasClass('active') ){
-            $active[0].play();
-        }else if(!$default.hasClass('active')){
-            $default.find('video')[0].pause();
-        }
-    });
+
+    // var videos = document.querySelectorAll('video.video-fluid');
+    // for(var i = 0; i < videos.length; i++) {
+    //     videos[i].pause();
+    //     videos[i].currentTime = 0;
+    // }
+
+
 
 
 
@@ -31,16 +51,36 @@ $(document).ready(function () {
         $(this).addClass("active");
     });
 
-    var myDiv = $(".img-room");
-    $(window).resize(function () {
-        var myChild = myDiv.find("> *:first-child");
-        myDiv.animate({
-            scrollLeft: (myChild.width() - myDiv.width()) / 2
-        }, {
-            duration: 0
-        });
+
+    $myDiv.scroll(function() {
+        $handBox.hide(100);
     });
-    $(window).resize();
+    let iScrollPos = 0;
+    $(window).scroll(function () {
+        let iCurScrollPos = $(this).scrollTop();
+        if (iCurScrollPos > iScrollPos) {
+            $nav.addClass('fold-active')
+        } else {
+            $nav.removeClass('fold-active')
+        }
+        iScrollPos = iCurScrollPos;
+    });
+    // function scroll() {
+    //     if ($(this).scrollTop() > 400) {
+    //         $nav.addClass('fold-active')
+    //         // $nav.slideUp(2000);
+    //         // $navigation.addClass("scroll-nav");
+    //     } else if($(this).scrollTop() > 300) {
+    //         $nav.removeClass('fold-active')
+    //         // $nav.slideDown(2000);
+    //         // $nav.removeClass("f-nav");
+    //         // $navigation.removeClass("scroll-nav");
+    //     }
+    // }
+
+    window.addEventListener("load", scroll);
+    window.addEventListener("scroll", scroll);
+
 
     $(window).on("load resize scroll", function (e) {
         let $windowTop = $(window).scrollTop();
